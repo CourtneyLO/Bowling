54029e73b806fb45b04a4273c87bbafb4084e854
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-04-08 11:11:24 +0100

    returns perfect game for total points of 300

--- spec/resultSpec.js
+++ spec/resultSpec.js
@@ -1,14 +1,16 @@
 describe("Result", function() {
   var result;
-  var scoreBoard;
 
   beforeEach(function() {
     result = new Result;
-    scoreBoard = new ScoreBoard;
   });
 
   it ("returns gutter game when no pins are knocked down the whole game", function() {
     expect(result.displayResult(0)).toEqual("Gutter Game")
+  });
+
+  it ("return perfect score when 300 points are scored", function() {
+    expect(result.displayResult(300)).toEqual("Perfect Score")
   });
 
 });
--- src/result.js
+++ src/result.js
@@ -5,5 +5,7 @@ function Result() {
 Result.prototype.displayResult = function(scoreTotal) {
   if (scoreTotal === 0) {
     return "Gutter Game"
+  } else if (scoreTotal === 300) {
+    return "Perfect Score"
   }
 };
