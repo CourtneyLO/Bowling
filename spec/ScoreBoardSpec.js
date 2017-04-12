259e1d8dd3fd5e2d33d1287a3baa61bd0b004861
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-04-12 10:13:02 +0100

    Allows an extra roll for spare

--- spec/ScoreBoardSpec.js
+++ spec/ScoreBoardSpec.js
@@ -15,52 +15,41 @@ describe("ScoreBoard", function() {
     expect(scoreBoard.scores).toEqual([]);
   });
 
-  it ('initializes with a result of 0', function() {
-    expect(scoreBoard.result).toEqual(0);
+  it ('initializes with a max roll number of 20 - without bonus roll', function() {
+    expect(scoreBoard.MAXROLLS).toEqual(20);
   });
-
-  it ('initializes with a last roll of 0', function() {
-    expect(scoreBoard.currentFrame).toEqual([]);
-  });
-
-  it ('initializes with a max score constant', function() {
-    expect(scoreBoard.MAXSCORE).toEqual(10);
-  });
-
-  it ('initializes with a mimum score constant', function() {
-    expect(scoreBoard.MINSCORE).toEqual(0);
-  });
-
-  it ('initializes with a set turns per frame of 2', function() {
-    expect(scoreBoard.TURNSPERFRAME).toEqual(2);
-  });
+  //
+  // it ('initializes with bonus roll of 21 rolls', function() {
+  //   expect(scoreBoard.BONUSROLL).toEqual(21);
+  // });
 
   it ("convert score into a number", function() {
     expect(scoreBoard._convertToNumber("5")).toEqual(5);
   })
 
+  it ("adds first score to score array", function() {
+    scoreBoard.firstRoll("2");
+    expect(scoreBoard.scores).toEqual([2]);
+  });
+
   it ("adds second roll to scores array", function() {
     scoreBoard.firstRoll("5");
     scoreBoard.firstRoll("3")
-    expect(scoreBoard.scores).toEqual([[5, 3]]);
+    expect(scoreBoard.scores).toEqual([5, 3]);
   });
 
-  it ("adds first score to current frame", function() {
-    scoreBoard.firstRoll("2");
-    expect(scoreBoard.currentFrame).toEqual([2]);
-  });
 
   it ("has a score array of four scores", function() {
     scoreBoard.firstRoll("2");
     scoreBoard.firstRoll("3");
     scoreBoard.firstRoll("4");
     scoreBoard.firstRoll("5");
-    expect(scoreBoard.scores).toEqual([[2,3],[4, 5]]);
+    expect(scoreBoard.scores).toEqual([2, 3, 4, 5]);
   });
 
   it ("adds a strike to the scores array", function() {
     scoreBoard.firstRoll("10");
-    expect(scoreBoard.scores).toEqual([[10, 0]]);
+    expect(scoreBoard.scores).toEqual([10]);
   });
 
   it ("returns a two element array for 9 frames and a three element array for strike", function() {
@@ -70,10 +59,10 @@ describe("ScoreBoard", function() {
          i++;
        }
        while (i <= 11);
-    expect(scoreBoard.scores).toEqual([[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,10,10]])
+    expect(scoreBoard.scores).toEqual([10,10,10,10,10,10,10,10,10,10,10,10])
   });
 
-  it ("returns a two element array for 10 frames", function() {
+  it ("returns a two element array for 10 frames - no bonus ball allowed", function() {
     var i = 0;
        do {
          scoreBoard.firstRoll("1");
@@ -82,20 +71,41 @@ describe("ScoreBoard", function() {
        while (i <= 17);
     scoreBoard.firstRoll("1");
     scoreBoard.firstRoll("1");
-    expect(scoreBoard.scores).toEqual([[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1]]);
+    expect(scoreBoard.scores).toEqual([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
   });
 
   it ("returns a two element array for 9 frames and a three element array for spare", function() {
-    var i = 0;
-         do {
-           scoreBoard.firstRoll(1);
-           i++;
-         }
-         while (i <= 17);
+    scoreBoard.firstRoll("1");
+    scoreBoard.firstRoll("1");
+
+    scoreBoard.firstRoll("1");
+    scoreBoard.firstRoll("1");
+
+    scoreBoard.firstRoll("1");
+    scoreBoard.firstRoll("1");
+
+    scoreBoard.firstRoll("1");
+    scoreBoard.firstRoll("1");
+
+    scoreBoard.firstRoll("1");
+    scoreBoard.firstRoll("1");
+
+    scoreBoard.firstRoll("1");
+    scoreBoard.firstRoll("1");
+
+    scoreBoard.firstRoll("1");
+    scoreBoard.firstRoll("1");
+
+    scoreBoard.firstRoll("1");
+    scoreBoard.firstRoll("1");
+
+    scoreBoard.firstRoll("1");
+    scoreBoard.firstRoll("1");
+
     scoreBoard.firstRoll("7");
     scoreBoard.firstRoll("3");
     scoreBoard.firstRoll("1");
-    expect(scoreBoard.scores).toEqual([[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[1, 1],[7, 3, 1]]);
+    expect(scoreBoard.scores).toEqual([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7, 3, 1]);
   });
 
   it ("returns an error that the scoreboard is full after 10 frames have been bowled", function() {
@@ -104,7 +114,7 @@ describe("ScoreBoard", function() {
            scoreBoard.firstRoll("10");
           i++;
         }
-        while (i <= 11);
+        while (i <= 19);
         expect( function(){ scoreBoard.firstRoll("7") } ).toThrow(new Error("Game is over, please start a new game"))
       });
 
--- src/ScoreBoard.js
+++ src/ScoreBoard.js
@@ -4,16 +4,11 @@ function ScoreBoard(frameCount, displayScores) {
   this.frameCount = frameCount;
   this.displayScores = displayScores
   this.scores = [];
-  this.result = 0;
-  this.currentFrame = [];
-  this.MAXSCORE = 10;
-  this.MINSCORE = 0;
-  this.TURNSPERFRAME = 2;
-  this.MAXTURNSPERFRAME = 3;
+  this.MAXROLLS = 20;
 }
 
 ScoreBoard.prototype.firstRoll = function(amount) {
-  if (this.scores.length < 10 ) {
+  if (this.scores.length < this.MAXROLLS || this.scores[this.scores.length - 1] +  this.scores[this.scores.length - 2] === 10 ) {
     this.displayScores.score(amount);
     this.currentScore(amount);
   } else {
@@ -25,7 +20,6 @@ ScoreBoard.prototype.firstRoll = function(amount) {
 ScoreBoard.prototype.currentScore = function(amount) {
   var score = this._convertToNumber(amount);
   this._inputCurrentScore(score)
-  this._addCompletedFrame()
 };
 
 ScoreBoard.prototype.startNewGame = function() {
@@ -37,51 +31,5 @@ ScoreBoard.prototype._convertToNumber = function(amount) {
 };
 
 ScoreBoard.prototype._inputCurrentScore = function(score) {
-  if (score === this.MAXSCORE && this.frameCount.count < 10) {
-    this.currentFrame.push(score, this.MINSCORE)
-  } else {
-    this.currentFrame.push(score)
-  }
+    return this.scores.push(score)
 };
-
-ScoreBoard.prototype._resetCurrentFrame =  function() {
-  if (this._completeFrame()) {
-    this.currentFrame = [];
-  }
-};
-
-ScoreBoard.prototype._addCompletedFrame = function() {
-  if (this._completeFrame()) {
-    this.scores.push(this.currentFrame);
-  }
-  this._resetCurrentFrame()
-  this.frameCount.increaseCount()
-};
-
-ScoreBoard.prototype._completeFrame = function() {
-  if (this.frameCount.count === 10 && (this._strike() || this._spare()) ) {
-    return this.currentFrame.length === this.MAXTURNSPERFRAME;
-  } else {
-    return this.currentFrame.length === this.TURNSPERFRAME;
-  }
-};
-
-ScoreBoard.prototype._spare = function() {
-  return this.currentFrame[0] + this.currentFrame[1] === this.MAXSCORE
-};
-
-ScoreBoard.prototype._strike = function() {
-  return this.currentFrame[0] === this.MAXSCORE;
-};
-
-// ScoreBoard.prototype._displayScore = function(amount) {
-//   if (amount === '10') {
-//     return "X";
-//   } else if (this._spare()) {
-//     return '/'
-//   } else if (amount === '0') {
-//     return "-"
-//   } else {
-//     return amount;
-//   }
-// };
