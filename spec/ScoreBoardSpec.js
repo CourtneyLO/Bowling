575f6b89914818a4070e45f32945f13d9c05dfdf
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-04-12 19:53:39 +0100

    splits string with 9s and 0s

--- spec/ScoreBoardSpec.js
+++ spec/ScoreBoardSpec.js
@@ -9,3 +9,7 @@ describe("ScoreBoard", function() {
   it ("splits string of strikes into array", function() {
     expect(scoreBoard.splitScores("X|X|X|X|X|X|X|X|X|X||XX")).toEqual(["X","X","X","X","X","X","X","X","X","X","X","X"]);
   })
+
+  it ("splits string of 0s and 9s into array", function() {
+    expect(scoreBoard.splitScores("9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||")).toEqual(["9","-","9","-","9","-","9","-","9","-","9","-","9","-","9","-","9","-","9","-"]);
+  })
