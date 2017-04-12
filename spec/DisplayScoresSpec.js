b3e70799ddc0e5a81802c67cedada0dbd6965029
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-04-12 22:50:58 +0100

    returns score in html

--- spec/DisplayScoresSpec.js
+++ spec/DisplayScoresSpec.js
@@ -5,56 +5,16 @@ describe("DisplayScore", function() {
 
   beforeEach(function() {
     displayScores = new DisplayScores();
-
   });
 
-  it ("displays 'X' for a strike", function() {
-    expect(displayScores.score("10")).toMatch('<td>X</td>');
+  it ("returns a value of 300 for a perfect score", function() {
+    var totalScore = 300
+    expect(displayScores.total(totalScore)).toEqual('<p>300</p>')
   });
 
-  it ("displays '-' for a miss", function() {
-    expect(displayScores.score("0")).toMatch("<td>-</td>")
+  it ("returns a score of 90", function() {
+    var totalScore = 90
+    expect(displayScores.total(totalScore)).toEqual('<p>90</p>')
   });
 
-  it("displays 2 for a score of 2", function(){
-    expect(displayScores.score("2")).toMatch("<td>2</td>")
-  });
-
-  it ("displays '/' for a spare", function() {
-    displayScores.score("2");
-    expect(displayScores.score("8")).toMatch("/");
-  });
-
-  it ("resets log after two throws", function() {
-    displayScores.score("2");
-    displayScores.score("8");
-    displayScores.score("5");
-    expect(displayScores.score("5")).toMatch("/");
-  });
-
-  it ("displays a full score board", function() {
-    displayScores.score("2");
-    displayScores.score("8");
-    displayScores.score("0");
-    displayScores.score("6");
-    displayScores.score("10");
-    displayScores.score("8");
-    displayScores.score("2");
-    displayScores.score("7");
-    displayScores.score("3");
-    displayScores.score("6");
-    displayScores.score("1");
-    displayScores.score("10");
-    displayScores.score("6");
-    displayScores.score("1");
-    displayScores.score("5");
-    displayScores.score("5");
-    displayScores.score("1");
-    expect(displayScores.score("5")).toMatch('<td>2</td><td>/</td><td>-</td><td>6</td><td>X</td><td>8</td><td>/</td><td>7</td><td>/</td><td>6</td><td>1</td><td>X</td><td>6</td><td>1</td><td>5</td><td>/</td><td>1</td><td>5</td>');
-  });
-
-  it("displays a final result of 29", function() {
-    var totalScore = 29;
-    expect(displayScores.total(totalScore)).toMatch('<td>29</td>')
-  });
 });
--- src/DisplayScores.js
+++ src/DisplayScores.js
@@ -1,49 +1,9 @@
 'use-strict';
 
 function DisplayScores() {
-  this.currentFrame = [];
-};
-
-DisplayScores.prototype.score = function(score) {
-    return this.displayScore(score);
-}
-
-DisplayScores.prototype.displayScore = function(score) {
-  if (score === "10") {
-    return this._setDisplay("X")
-  } else {
-    return this._addScoreToCurrentFrame(score)
-  }
 };
 
 DisplayScores.prototype.total = function(totalScore) {
-  var total = document.getElementById("bowlingTotal")
-  return total.innerHTML = '<td>' + totalScore + '</td>'
+  var total = document.getElementById("bowlingScores")
+  return total.innerHTML = '<p>' + totalScore + '</p>'
 }
-
-DisplayScores.prototype._addScoreToCurrentFrame = function(score) {
-  this._resetCurrentFrame()
-  this.currentFrame.push(Number(score))
-  return this._workingOutSpare(score)
-}
-
-DisplayScores.prototype._workingOutSpare = function(score) {
-  if (this.currentFrame.reduce((a, b) => a + b, 0) === 10) {
-    return this._setDisplay("/")
-  } else if (score === "0") {
-    return this._setDisplay("-")
-  } else {
-    return this._setDisplay(score)
-  }
-};
-
-DisplayScores.prototype._resetCurrentFrame = function() {
-  if(this.currentFrame.length > 1) {
-    this.currentFrame = [];
-  }
-};
-
-DisplayScores.prototype._setDisplay = function(element) {
-  var score = document.getElementById("bowlingScores")
-  return score.innerHTML += "<td>" + element + "</td>";
-};
