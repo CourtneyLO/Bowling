6e8b7ed3e9ad7365fa8877564c8d7f248c6ed83b
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-04-12 08:28:21 +0100

    initializes with a result of zero

--- spec/CalculateScoreSpec.js
+++ spec/CalculateScoreSpec.js
@@ -8,78 +8,7 @@ describe('ScoreCalculator', function(){
   });
 
   it ("initializes with an empty result array", function(){
-    expect(scoreCalculator.result).toEqual([]);
+    expect(scoreCalculator.result).toEqual(0);
   });
-
-  it("initializes with a scores array", function() {
-    expect(scoreCalculator.scores).toEqual([]);
-  });
-
-  it ("returns a score of 67", function() {
-    var scores = [[2,3],[4,2],[5,2],[5,1],[8,1],[3,3],[2,3],[4,3],[6,1],[7,2]];
-    scoreCalculator.calculate(scores);
-    expect(scoreCalculator.result).toEqual(67);
-  });
-
-  it ("returns a score of 70 with one spare", function() {
-    var scores = [[2,3],[5,1],[5,5],[2,2],[8,1],[3,3],[2,3],[4,3],[6,1],[7,2]];
-    scoreCalculator.calculate(scores);
-    expect(scoreCalculator.result).toEqual(70)
-  });
-
-  it("returns a score of 81 with a spare and a strike", function() {
-    var scores = [[2,3],[5,1],[5,5],[2,2],[8,1],[3,4],[10,0],[3,3],[6,1],[7,2]];
-    scoreCalculator.calculate(scores);
-    expect(scoreCalculator.result).toEqual(81);
-  });
-
-  it("returns a score of 120 with a mixture of strikes and spares consecuatively", function() {
-    var scores = [[2,3],[2,2],[5,5],[10,0],[7,3],[3,3],[10,0],[8,2],[6,1],[7,2]];
-    scoreCalculator.calculate(scores);
-    expect(scoreCalculator.result).toEqual(120);
-  });
-
-  it("returns a score of 183 made up mostly of spares and strikes", function() {
-    var scores = [[5,5],[10,0],[7,3],[6,4],[8,2],[10,0],[10,0],[6,4],[9,1],[5,2],[0]];
-    scoreCalculator.calculate(scores);
-    expect(scoreCalculator.result).toEqual(181);
-  });
-
-  it("returns 257 for a full board of strikes except the last two throws", function() {
-    var scores = [[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[3,4]];
-    scoreCalculator.calculate(scores);
-    expect(scoreCalculator.result).toEqual(257);
-  });
-
-  it("returns a total score with the bonus roll included", function() {
-    var scores = [[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[4,5],[10,10,10]];
-    scoreCalculator.calculate(scores)
-    expect(scoreCalculator.result).toEqual(262);
-  });
-
-  it("returns a total score with the bonus roll included", function() {
-    var scores = [[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[7,3],[10,0],[7,3,1]];
-    scoreCalculator.calculate(scores)
-    expect(scoreCalculator.result).toEqual(248);
-  });
-
-  it("returns a total score with a spare in the bonus roll", function() {
-    var scores = [[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[7,3],[5,5],[7,3,1]];
-    scoreCalculator.calculate(scores)
-    expect(scoreCalculator.result).toEqual(240);
-  });
-
-  it("returns a total score with a strike in the bonus roll", function() {
-    var scores = [[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[7,3],[5,5],[10,10,10]];
-    scoreCalculator.calculate(scores)
-    expect(scoreCalculator.result).toEqual(262);
-  });
-
-  it("returns a total score with the bonus roll included", function() {
-    var scores = [[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,10,10]];
-    scoreCalculator.calculate(scores)
-    expect(scoreCalculator.result).toEqual(300);
-  });
-
 
 });
--- src/CalculateScore.js
+++ src/CalculateScore.js
@@ -1,26 +1,5 @@
 'use-strict'
 
 function ScoreCalculator() {
-  this.result = [];
-  this.scores = [];
-};
-
-ScoreCalculator.prototype.calculate = function(scores) {
-  for(var num = 0; num < scores.length; num ++) {
-    for(var i = 0; i < scores[num].length; i ++) {
-      if (scores[num][i] === 10 && scores[num].length < 3) {
-        scores[num] = scores[num][i] + scores[num + 1].reduce((a, b) => a + b, 0)
-        if (scores[num + 1][i] === 10) {
-          console.log(scores[num + 2])
-          scores[num] = scores[num] + scores[num + 2][i]
-        }
-      } else if (scores[num][i] + scores[num][i + 1]  === 10) {
-        scores[num] = [scores[num][i] + scores[num][1] + scores[num + 1][i]]
-      } else {
-        scores
-      }
-      var allResults = [].concat.apply([], scores)
-      this.result = allResults.reduce((a, b) => a + b, 0)
-    }
-  }
+  this.result = 0;
 };
