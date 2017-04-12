1a3508fefd7218189b13a6c6e63dbce0749dd2ed
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-04-12 19:52:56 +0100

    splits string with strikes

--- src/ScoreBoard.js
+++ src/ScoreBoard.js
@@ -1,61 +1,16 @@
 'use-strict';
 
-function ScoreBoard(frameCount, displayScores, scoreCalculator) {
-  this.frameCount = frameCount;
-  this.displayScores = displayScores;
-  this.scoreCalculator = scoreCalculator;
-  this.scores = [];
-  this.MAXROLLS = 20;
-  this.BONUSBALL = 1;
+function ScoreBoard() {
+
+};
+
+ScoreBoard.prototype.splitScores = function(score) {
+  var scores = [];
+  score = score.split("")
+  for (var i = 0; i < score.length; i++) {
+  if (score[i] && score[i] !== '|') {
+    scores.push(score[i]);
+  }
 }
-
-ScoreBoard.prototype.rollScore = function(score) {
-  if (this._convertToNumber(score) <= 10) {
-    return this._useScore(score);
-  } else {
-    throw new Error("Score is invalid, please try again")
-  }
-};
-
-ScoreBoard.prototype._useScore = function(score) {
-  if (this.scores.length < this._isThrowAllowed()) {
-    this.displayScores.score(score);
-    this._currentScore(score);
-  } else {
-    throw new Error("Game is over, please start a new game");
-  }
-};
-
-ScoreBoard.prototype._isThrowAllowed = function() {
-  if (this.scores.length >= 20 && this._bonusBall()) {
-    return this.MAXROLLS += this.BONUSBALL;
-  } else {
-    return this.MAXROLLS
-  }
-};
-
-ScoreBoard.prototype.getTotalScore = function() {
-  var result = this.scoreCalculator.calculate(this.scores);
-  return this.displayScores.total(result);
-};
-
-ScoreBoard.prototype.startNewGame = function() {
-  return new ScoreBoard(this.frameCount);
-};
-
-ScoreBoard.prototype._currentScore = function(amount) {
-  var score = this._convertToNumber(amount);
-  this._inputCurrentScore(score)
-};
-
-ScoreBoard.prototype._convertToNumber = function(amount) {
-  return Number(amount);
-};
-
-ScoreBoard.prototype._inputCurrentScore = function(score) {
-    return this.scores.push(score)
-};
-
-ScoreBoard.prototype._bonusBall = function() {
-  return this.scores[this.scores.length - 1] +  this.scores[this.scores.length - 2] >= 10
-};
+return scores
+}
