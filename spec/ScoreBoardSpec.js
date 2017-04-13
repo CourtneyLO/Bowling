9b3d6776cadd7291d9e3790273fff7ca3c6c9032
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-04-13 06:33:48 +0100

    Adds possible scores constant

--- spec/ScoreBoardSpec.js
+++ spec/ScoreBoardSpec.js
@@ -5,63 +5,68 @@ describe("ScoreBoard", function() {
 
 
   beforeEach(function() {
-    displayScore = new DisplayScore();
-    scoreCalculator = new ScoreCalculator();
-    scoreBoard = new ScoreBoard(scoreCalculator, displayScore);
+    scoreBoard = new ScoreBoard();
   });
 
   describe("initializes with:" , function() {
-    it ("an empty scores array", function() {
-      expect(scoreBoard.scores).toEqual([]);
-    });
 
-    it ("a maximum score per role constant of 10", function() {
-      expect(scoreBoard.MAXSCOREPERROLL).toEqual(10);
-    });
-
-    it ("a minimum score per role constant of 0", function() {
-      expect(scoreBoard.MINSCOREPERROLL).toEqual(0);
+    it("a constant of possible score results", function() {
+      expect(scoreBoard.SCORESYMBOLS).toEqual([{'X': 10,
+                                                '/': 10,
+                                                '-': 0}])
     });
   });
+  //   it ("an empty scores array", function() {
+  //     expect(scoreBoard.scores).toEqual([]);
+  //   });
+  //
+  //   it ("a maximum score per role constant of 10", function() {
+  //     expect(scoreBoard.MAXSCOREPERROLL).toEqual(10);
+  //   });
+  //
+  //   it ("a minimum score per role constant of 0", function() {
+  //     expect(scoreBoard.MINSCOREPERROLL).toEqual(0);
+  //   });
 
-
-  describe("splits string of:" , function() {
-    it ("strikes into array", function() {
-      expect(scoreBoard.splitScores("X|X|X|X|X|X|X|X|X|X||XX")).toEqual([10,10,10,10,10,10,10,10,10,10,10,10]);
-    });
-
-    it ("0s and 9s into array", function() {
-      expect(scoreBoard.splitScores("9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||")).toEqual([9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0]);
-    });
-
-    it ("spares", function() {
-      expect(scoreBoard.splitScores("5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5")).toEqual([5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]);
-    });
-
-    it ("a mixture of scores", function() {
-      expect(scoreBoard.splitScores("X|7/|9-|X|-8|8/|-6|X|X|X||81")).toEqual([10,7,3,9,0,10,0,8,8,2,0,6,10,10,10,8,1]);
-    });
-  });
-
-  describe("returns score of:" , function() {
-    it ("300 for a perfect score", function() {
-      scoreBoard.splitScores("X|X|X|X|X|X|X|X|X|X||XX")
-      expect(scoreBoard.displayTotal()).toEqual('<p>300</p>')
-    });
-
-    it ("90 containing 0s and 9s", function() {
-      scoreBoard.splitScores("9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||")
-      expect(scoreBoard.displayTotal()).toEqual('<p>90</p>')
-    });
-
-    it ("150 containing spares", function() {
-      scoreBoard.splitScores("5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5")
-      expect(scoreBoard.displayTotal()).toEqual('<p>150</p>')
-    });
-
-    it ("167 containing a mixture of scores", function() {
-      scoreBoard.splitScores("X|7/|9-|X|-8|8/|-6|X|X|X||81")
-      expect(scoreBoard.displayTotal()).toEqual('<p>167</p>')
-    });
-  });
+  //
+  //
+  // describe("splits string of:" , function() {
+  //   it ("strikes into array", function() {
+  //     expect(scoreBoard.splitScores("X|X|X|X|X|X|X|X|X|X||XX")).toEqual([10,10,10,10,10,10,10,10,10,10,10,10]);
+  //   });
+  //
+  //   it ("0s and 9s into array", function() {
+  //     expect(scoreBoard.splitScores("9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||")).toEqual([9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0]);
+  //   });
+  //
+  //   it ("spares", function() {
+  //     expect(scoreBoard.splitScores("5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5")).toEqual([5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]);
+  //   });
+  //
+  //   it ("a mixture of scores", function() {
+  //     expect(scoreBoard.splitScores("X|7/|9-|X|-8|8/|-6|X|X|X||81")).toEqual([10,7,3,9,0,10,0,8,8,2,0,6,10,10,10,8,1]);
+  //   });
+  // });
+  //
+  // describe("returns score of:" , function() {
+  //   it ("300 for a perfect score", function() {
+  //     scoreBoard.splitScores("X|X|X|X|X|X|X|X|X|X||XX")
+  //     expect(scoreBoard.displayTotal()).toEqual('<p>300</p>')
+  //   });
+  //
+  //   it ("90 containing 0s and 9s", function() {
+  //     scoreBoard.splitScores("9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||")
+  //     expect(scoreBoard.displayTotal()).toEqual('<p>90</p>')
+  //   });
+  //
+  //   it ("150 containing spares", function() {
+  //     scoreBoard.splitScores("5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5")
+  //     expect(scoreBoard.displayTotal()).toEqual('<p>150</p>')
+  //   });
+  //
+  //   it ("167 containing a mixture of scores", function() {
+  //     scoreBoard.splitScores("X|7/|9-|X|-8|8/|-6|X|X|X||81")
+  //     expect(scoreBoard.displayTotal()).toEqual('<p>167</p>')
+  //   });
+  // });
 });
--- src/ScoreBoard.js
+++ src/ScoreBoard.js
@@ -1,41 +1,7 @@
 'use-strict';
 
-function ScoreBoard(scoreCalculator, displayScore) {
-  this.scoreCalculator = scoreCalculator;
-  this.displayScore = displayScore;
-  this.scores = [];
-  this.MAXSCOREPERROLL = 10;
-  this.MINSCOREPERROLL = 0;
-};
-
-ScoreBoard.prototype.splitScores = function(score) {
-  individulaScores = score.split("")
-  for (var scoreIndex = 0; scoreIndex < score.length; scoreIndex++) {
-    if (individulaScores[scoreIndex] !== '|') {
-      this._convertToNumber(scoreIndex);
-    }
-  };
-    return this.scores
-};
-
-ScoreBoard.prototype._convertToNumber = function(scoreIndex) {
-  if (individulaScores[scoreIndex] === 'X') {
-     this.scores.push(this.MAXSCOREPERROLL);
-  } else if (individulaScores[scoreIndex] === '-') {
-      this.scores.push(this.MINSCOREPERROLL)
-  } else if (individulaScores[scoreIndex] === '/'){
-     this.scores.push(this.MAXSCOREPERROLL - individulaScores[scoreIndex - 1])
-  } else {
-     this.scores.push(Number(individulaScores[scoreIndex]))
-  }
-}
-
-ScoreBoard.prototype.displayTotal = function() {
-  var result =  this.scoreCalculator.calculate(this.scores)
-  this._refreshScores();
-  return this.displayScore.total(result)
-}
-
-ScoreBoard.prototype._refreshScores = function() {
-  this.scores = []
+function ScoreBoard() {
+  this.SCORESYMBOLS = [{'X': 10,
+                        '/': 10,
+                        '-': 0}]
 };
